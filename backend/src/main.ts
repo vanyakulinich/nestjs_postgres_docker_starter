cded395663fff8b77003348eafcb7cf77ab31ef8
vanyakulinich <kulinichvanya@gmail.com>
2021-02-06 23:34:58 +0200

    ops(backend): api swagger config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const API_PREFIX = 'api' // global prefix for api
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix(API_PREFIX)
+
+  // Setting up Swagger document
+  const options = new DocumentBuilder()
+    .setTitle('FilesHub API')
+    .setDescription('FilesHub application API docs')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+
+  SwaggerModule.setup(`${API_PREFIX}/docs`, app, document)
+
   await app.listen(process.env.PORT)
 }
 bootstrap()
