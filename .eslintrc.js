ca77fb7be4ff75a6f003f6f14d1bcbeb98997239
vanyakulinich <kulinichvanya@gmail.com>
2021-03-03 21:27:32 +0200

    refactor: cleanups

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,11 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'postgres*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
